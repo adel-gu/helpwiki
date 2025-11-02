ed0149e8ca0d2b7aea6e1509b1ce9a9ba1c38221
adel <adeelguitoun@gmail.com>
2025-11-02 17:27:27 +0100

    Implemented founder onboarding path

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,31 +1,17 @@
 class WorkspacesController < ApplicationController
-  before_action :set_workspace, only: %i[ show edit update destroy ]
-
-  # GET /workspaces or /workspaces.json
-  def index
-    @workspaces = Workspace.all
-  end
-
-  # GET /workspaces/1 or /workspaces/1.json
-  def show
-  end
-
   # GET /workspaces/new
   def new
     @workspace = Workspace.new
   end
 
-  # GET /workspaces/1/edit
-  def edit
-  end
-
   # POST /workspaces or /workspaces.json
   def create
     @workspace = Workspace.new(workspace_params)
+    current_user.update(workspace: @workspace, role: 4)
 
     respond_to do |format|
       if @workspace.save
-        format.html { redirect_to @workspace, notice: "Workspace was successfully created." }
+        format.html { redirect_to app_root_path(@workspace.subdomain), notice: "Workspace was successfully created." }
         format.json { render :show, status: :created, location: @workspace }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,34 +20,7 @@ class WorkspacesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /workspaces/1 or /workspaces/1.json
-  def update
-    respond_to do |format|
-      if @workspace.update(workspace_params)
-        format.html { redirect_to @workspace, notice: "Workspace was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @workspace }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @workspace.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /workspaces/1 or /workspaces/1.json
-  def destroy
-    @workspace.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to workspaces_path, notice: "Workspace was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_workspace
-      @workspace = Workspace.find(params.expect(:id))
-    end
 
     # Only allow a list of trusted parameters through.
     def workspace_params
