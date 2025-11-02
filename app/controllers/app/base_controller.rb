f6bdd47c1a213b353d99466aa7a20e759307fcd1
adel <adeelguitoun@gmail.com>
2025-11-02 16:42:26 +0100

    Fix general layout

--- app/controllers/app/base_controller.rb
+++ app/controllers/app/base_controller.rb
@@ -1,8 +1,15 @@
 class App::BaseController < ApplicationController
   include Pundit::Authorization
-  set_current_tenant_by_subdomain(:workspace, :subdomain)
+  set_current_tenant_through_filter
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_tenant
 
   layout "app"
+
+  private
+
+    def set_tenant
+
+    end
+
 end
--- app/controllers/public/landing_controller.rb
+++
@@ -1,6 +0,0 @@
-class Public::LandingController < ApplicationController
-  layout "landing"
-
-  def index
-  end
-end
