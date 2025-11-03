6aee4b2524f8c34cdd427dcfd61d411391c27ca9
adel <adeelguitoun@gmail.com>
2025-11-03 21:33:26 +0100

    Add inviting actions

--- app/controllers/app/users_management_controller.rb
+++ app/controllers/app/users_management_controller.rb
@@ -5,6 +5,24 @@ class App::UsersManagementController < App::BaseController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.invite!(
+      { email: user_params[:email], role: user_params[:role], workspace: current_tenant },
+      current_user # inviter
+    )
+
+    if @user.errors.empty?
+      redirect_to app_users_management_index_path(current_tenant.subdomain), notice: "Invitation sent to #{@user.email}."
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   # PATCH /app/users_management/:id
   def update
     if @user.update(user_params)
@@ -30,6 +48,6 @@ class App::UsersManagementController < App::BaseController
   end
 
   def user_params
-    params.require(:user).permit(:role)
+    params.require(:user).permit(:email, :role)
   end
 end
