2f02cfafaf7dd7fcf5725e12b7f95d246808c5bf
adel <adeelguitoun@gmail.com>
2025-11-03 08:31:12 +0100

    Add validation, factories, and unit testing for workspace model

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,3 +1,7 @@
 class Workspace < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
+  validates :subdomain, presence: true, uniqueness: true,
+                        format: { with: /\A[a-z0-9\-]+\z/, message: 'only allows lowercase letters, numbers, and hyphens' }
 end
--- spec/models/workspace_spec.rb
+++ spec/models/workspace_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Workspace, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:workspace) }
+
+  it { should have_many(:users) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:subdomain) }
+  it { should validate_uniqueness_of(:subdomain) }
+  it { should allow_value("workspace").for(:subdomain) }
+  it { should_not allow_value("My workspace!").for(:subdomain) }
 end
