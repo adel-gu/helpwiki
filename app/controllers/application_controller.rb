bbe30ab62521340a4d129ff2936abbd9dd827d60
adel <adeelguitoun@gmail.com>
2025-11-03 11:23:52 +0100

    Add before_action for permitted parameters "full_name"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def after_sign_in_path_for(resource)
     if resource.workspace
@@ -8,5 +10,11 @@ class ApplicationController < ActionController::Base
     else
       new_workspace_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name ])
   end
 end
