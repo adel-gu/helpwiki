6928796a34796ffc5e05dce329ffe508229d5051
adel <adeelguitoun@gmail.com>
2025-11-06 17:20:28 +0100

    Generated the ActivityLog model and migration.

--- app/models/article_version.rb
+++ app/models/article_version.rb
@@ -2,5 +2,5 @@ class ArticleVersion < ApplicationRecord
   acts_as_tenant :workspace
 
   belongs_to :article
-  belongs_to :author, class_name: 'User', optional: true
+  belongs_to :author, class_name: 'User'
 end
