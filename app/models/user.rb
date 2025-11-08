730fe6be38cb0a80928c3201c5d230145b0357bc
adel <adeelguitoun@gmail.com>
2025-11-08 10:42:22 +0100

    Refactor User model: reorganize and define associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ApplicationRecord
-  acts_as_tenant :workspace, optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  # -- Associations --
+  acts_as_tenant :workspace, optional: true
+
+  # Articles this user created, is writing, or is reviewing
+  has_many :created_articles, class_name: "Article", foreing_key: :creator_id, dependent: :nullify
+  has_many :written_articles, class_name: "Article", foreing_key: :writer_id, dependent: :nullify
+  has_many :reviewed_articles, class_name: "Article", foreing_key: :reviewer_id, dependent: :nullify
+
+  # Category
+  has_many :categories, dependent: :nullify
+
+  # Article version
+  has_many :article_versions, class_name: "Article_versions", foreign_key: :author_id, dependent: :nullify
+
+  # Activity log
+  has_many :activity_logs, class_name: "ActivityLogs", foreign_key: :actor_id, dependent: :nullify
+
+  # -- Validation --
   enum :role, { reader: 1, collaborator: 2, editor: 3, admin: 4 }
 
   validates :full_name, presence: true, length: { maximum: 100 }, unless: :invitation_created?
