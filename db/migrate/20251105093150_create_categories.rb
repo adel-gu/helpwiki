fa1f86f1a2170fec2f1869f489dd2e6d98c96d30
adel <adeelguitoun@gmail.com>
2025-11-08 10:54:04 +0100

    Add creator association to Articles and ensure Category reference is optional

--- db/migrate/20251105093150_create_categories.rb
+++ db/migrate/20251105093150_create_categories.rb
@@ -4,6 +4,7 @@ class CreateCategories < ActiveRecord::Migration[8.0]
       t.references :workspace, null: false, foreign_key: true
       t.string :name, null: false
       t.references :parent_category, foreign_key: { to_table: :categories }, index: true
+      t.references :creator, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/migrate/20251106155254_create_articles.rb
+++ db/migrate/20251106155254_create_articles.rb
@@ -10,7 +10,8 @@ class CreateArticles < ActiveRecord::Migration[8.0]
       t.string :slug
       t.datetime :published_at
       t.references :workspace, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
+      t.references :category, foreign_key: true
+      t.references :creator, foreign_key: { to_table: :users }
       t.references :writer, foreign_key: { to_table: :users }
       t.references :reviewer, foreign_key: { to_table: :users }
 
