c3f2dae179df03e69006abaccb3afb511757e954
adel <adeelguitoun@gmail.com>
2025-11-08 10:47:30 +0100

    Enhance Category model: add creator association and update articles relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,11 @@
 class Category < ApplicationRecord
   acts_as_tenant :workspace
+
+  belongs_to :creator, class_name: "User"
   belongs_to :parent_category, class_name: "Category", optional: true
+
   has_many :sub_categories, class_name: "Category", foreign_key: "parent_category_id", dependent: :destroy
-  has_many :articles
+  has_many :articles, dependent: :nullify
 
   validates :name, presence: true
 end
